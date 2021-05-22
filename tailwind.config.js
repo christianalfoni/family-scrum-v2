423160c8f4a2fdf3d7ba0adfe5342df5c3b59825
Christian Alfoni <christianalfoni@gmail.com>
2021-05-22 11:39:27 +0200

    fix purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
