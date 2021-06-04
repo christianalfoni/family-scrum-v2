dd87dcf72eecaa277a385d29d7a8842c0c8069ac
Christian Alfoni <christianalfoni@gmail.com>
2021-06-04 12:56:59 +0000

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,13 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin'
 
-export const barcode = functions.https.onRequest((request, response) => {
+admin.initializeApp(functions.config().firebase);
+
+const BARCODE_TOKENS_COLLECTION = 'barcodeTokens'
+const FAMILY_DATA_COLLECTION = 'familyData'
+const FAMILY_BARCODES_COLLECTION = 'barcodes'
+
+export const barcode = functions.region('europe-west1').https.onRequest((request, response) => {
   if (request.method !== "POST") {
     throw new Error("Invalid method");
   }
@@ -18,5 +25,32 @@ export const barcode = functions.https.onRequest((request, response) => {
     { structuredData: true }
   );
 
-  response.send({ ok: true });
+
+
+  admin.firestore().collection(BARCODE_TOKENS_COLLECTION).doc(request.body.token)
+    .get()
+    .then((doc) => {
+      const data = doc.data()
+      if (!data) {
+        throw new Error("Token does not exist")
+      }
+
+      return data.familyId
+    })
+    .then((familyId) => {
+      return admin.firestore().collection(FAMILY_DATA_COLLECTION).doc(familyId).collection(FAMILY_BARCODES_COLLECTION)
+        .add({
+          created: admin.firestore.FieldValue.serverTimestamp(),
+          modified: admin.firestore.FieldValue.serverTimestamp(),
+          groceryId: null
+        })
+    })
+    .then(() => {
+      response.send({ ok: true });
+    })
+    .catch((error) => {
+      response.status(500).send({
+        error: error.message
+      })
+    })
 });
