90e4721932bb4f5e2a853479a0fb4a13031cdbc4
Christian Alfoni <christianalfoni@gmail.com>
2023-11-06 20:21:37 +0000

    Update 48 files

--- src/useGlobalContext/index.ts
+++ src/useGlobalContext/index.ts
@@ -2,15 +2,18 @@ import { context } from "impact-context";
 import { createFirebase } from "./firebase";
 import { createViews } from "./views";
 import { createAuthentication } from "./authentication";
+import { createCamera } from "./camera";
 
 export const useGlobalContext = context(() => {
   const firebase = createFirebase();
   const views = createViews();
   const authentication = createAuthentication(firebase);
+  const camera = createCamera();
 
   return {
     views,
     firebase,
     authentication,
+    camera,
   };
 });
