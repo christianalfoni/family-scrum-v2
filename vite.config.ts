c642a10117db5702b6471d44e62bbb4761a17b1e
Christian Alfoni <christianalfoni@gmail.com>
2025-02-14 08:34:51 +0100

    clean slate

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import observingComponents from "bonsify/babel-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
         plugins: [observingComponents()],
       },
     }),
+    tailwindcss(),
   ],
 });
