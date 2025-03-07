44d75386edc02e5c6a8c239413ff49da9092796f
Christian Alfoni <christianalfoni@gmail.com>
2025-03-07 20:58:38 +0100

    routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css";
 import Session from "./components/Session";
 import * as state from "./state";
 import { BrowserEnvironment } from "./environments/Browser";
+import { BrowserRouter } from "react-router";
 
 const env = BrowserEnvironment();
 const session = state.Session({ env });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Session session={session} />
+    <BrowserRouter>
+      <Session session={session} />
+    </BrowserRouter>
   </StrictMode>
 );
